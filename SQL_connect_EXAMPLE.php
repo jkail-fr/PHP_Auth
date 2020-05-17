27c9dbbb1129fcca6aceae14292c6b07bb084cc5
Jkail <boutaultbenoit@yahoo.fr>
2020-05-17 22:50:15 +0200

    debut de vérification de doublon email / pseudo lors de la création + en faire une fonction (fail pour le moment)

--- SQL_connect_EXAMPLE.php
+++ SQL_connect_EXAMPLE.php
@@ -4,3 +4,12 @@ $host='';
 $dbname ='';
 $login = '';
 $pwd = '';
+
+try {
+    $SQL = new PDO('mysql:host=' . $host .';dbname='. $dbname .';charset=utf8', $login, $pwd);
+} 
+catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
--- auth.php
+++ auth.php
@@ -10,15 +10,38 @@ if (empty($_POST))
     exit();
 } 
 elseif (isset($_POST['create'])) {
-        
-        $hashPassword=password_hash($_POST['password'], PASSWORD_BCRYPT);
-        $createUser = $SQL->prepare("INSERT INTO users(pseudo, userPassword, email) VALUES (:pseudo, :userPassword, :email )");
-        $createUser->execute(array(
-            'pseudo' => $_POST['pseudo'],
-            'userPassword' => $hashPassword,
-            'email' => $_POST['email']
-        ));
-        $_SESSION['pseudo'] = $_POST['pseudo'];
+    // Vérification de l'existence du pseudo puis de l'email dans la BDD avant d'éxecuter le code de création
+    $email = $_POST['email'];
+    $pseudo = $_POST['pseudo'];
+    /*
+    $ExistingPseudo = $SQL->prepare("SELECT pseudo from users WHERE pseudo=:pseudo");
+    $ExistingPseudo->execute(array('pseudo'=>$_POST['pseudo']));
+    $checkExistingPseudo = $ExistingPseudo->fetch(PDO::FETCH_OBJ);*/
+    $checkExistingPseudo = dbCheck($pseudo);
+    var_dump($checkExistingPseudo);
+    if ($checkExistingPseudo == false)
+       {
+        $ExistingEmail = $SQL->prepare("SELECT email from users WHERE email=:email");
+        $ExistingEmail->execute(array('email'=>$_POST['email']));
+        $checkExistingEmail = $ExistingEmail->fetch(PDO::FETCH_OBJ);
+        if ($checkExistingEmail == false)
+        {
+            $hashPassword=password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $createUser = $SQL->prepare("INSERT INTO users(pseudo, userPassword, email) VALUES (:pseudo, :userPassword, :email )");
+            $createUser->execute(array(
+                'pseudo' => $_POST['pseudo'],
+                'userPassword' => $hashPassword,
+                'email' => $_POST['email']
+            ));
+            $_SESSION['pseudo'] = $_POST['pseudo'];}
+        else {
+            echo "l'email ou le pseudo est existe déjà.";
+        }
+       }
+    else { 
+        echo "l'email ou le pseudo est existe déjà.";
+        }
+
 }
 else 
 {
